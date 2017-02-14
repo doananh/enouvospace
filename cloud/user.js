16d8431b609beabc70de99995137d754fcba690d
Minh Nguyen <minh.nguyen@enouvo.com>
2017-02-14 17:04:12 +0700

    Adding checkin api

--- cloud/user.js
+++ cloud/user.js
@@ -15,6 +15,68 @@ Parse.Cloud.define('checkLogin', function(request, response) {
   });
 });
 
+Parse.Cloud.define("checkin", function(req, res) {
+  var params = req.params;
+  var createNew;
+  var randomString = getRandomString();
+  var randomNumber = createRandomNumber();
+  var code = randomString.concat(randomNumber);
+  console.log(code)
+  if (code) {
+    if (params.UserId !== null) {
+      createNew = createNewBooking({ __type: "Pointer", className: "_User", objectId: params.UserId }, params, code);
+    } else {
+      createNew = createNewBooking(null, params, code);
+    }
+    Parse.Promise.when(createNew).then(
+    function(v1, v2) { res.success({ code: code}); },
+    function(error) { res.error(error); }
+  )
+  }
+});
+
+function createRandomNumber() {
+  var numbers = getRndInteger(0, 9);
+  var randomNumber = ""
+  for ( var i = 0; i<3; i++ ) {
+      randomNumber += numbers;
+  }
+  return randomNumber;
+}
+
+function getRandomString() {
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+  var randomstring = '';
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  randomstring += chars.substring(randomNumber,randomNumber+1);
+  return randomstring;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function createNewBooking(_userData, _params, _code) {
+  console.log(_userData);
+  var Booking = Parse.Object.extend("Booking");
+  var booking = new Booking();
+  var promise = new Parse.Promise();
+  booking.set("user", _userData);
+  booking.set("business", { __type: "Pointer", className: "Business", objectId: _params.BusinessId });
+  booking.set("package", { __type: "Pointer", className: "Package", objectId: _params.PackageId });
+  booking.set("packageCount", 1);
+  booking.set("code", _code);
+  booking.set("status", "Pending");
+  booking.set("isPaid", false);
+  booking.set("numOfUsers", 1);
+  booking.set("discount", null);
+  booking.save().then(
+    function(value) { promise.resolve(value); },
+    function(error) { promise.reject(error); }
+  )
+  return promise;
+}
+
 Parse.Cloud.define('deleteUser', function(request, response) {
   var query = new Parse.Query(Parse.User);
   query.get(request.params.objectId).then(function (user) {
