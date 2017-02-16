783ded555b35e5359f4a40eed7de704074851e60
Minh Nguyen <minh.nguyen@enouvo.com>
2017-02-16 14:36:31 +0700

    Change logic checkin api

--- cloud/user.js
+++ cloud/user.js
@@ -17,50 +17,70 @@ Parse.Cloud.define('checkLogin', function(request, response) {
 
 Parse.Cloud.define("checkin", function(req, res) {
   var params = req.params;
-  var createNew;
-  var randomString = getRandomString();
-  var randomNumber = createRandomNumber();
-  var code = randomString.concat(randomNumber);
-  console.log(code)
-  if (code) {
-    if (params.UserId !== null) {
-      createNew = createNewBooking({ __type: "Pointer", className: "_User", objectId: params.UserId }, params, code);
-    } else {
-      createNew = createNewBooking(null, params, code);
-    }
-    Parse.Promise.when(createNew).then(
-    function(v1, v2) { res.success({ code: code}); },
-    function(error) { res.error(error); }
-  )
+  var code;
+  if (params.UserId !== null) {
+    createNewBooking({ __type: "Pointer", className: "_User", objectId: params.UserId }, params, null).then(function (data) {
+      res.success({ status: 'Created'});
+    }, function (error) {
+      res.error(error);
+    });
+  } else {
+    getBooking().then(function (data) {
+      if (data.length > 0) {
+        var lastCode = data[data.length -1].get("code");
+        code = getCode(lastCode);
+      } else {
+        code = getCode('A000');
+      }
+      createNewBooking(null, params, code).then(function (data) {
+        res.success({ code: code});
+      }, function (error) {
+        res.error(error);
+      });
+    }, function (error) {
+    });
   }
 });
 
-function createRandomNumber() {
-  var numbers = getRndInteger(0, 9);
-  var randomNumber = ""
-  for ( var i = 0; i<3; i++ ) {
-      randomNumber += numbers;
-  }
-  return randomNumber;
-}
-
-function getRandomString() {
+function getCode(_lastCode) {
   var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
-  var randomstring = '';
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  randomstring += chars.substring(randomNumber,randomNumber+1);
-  return randomstring;
+  var firstChar, number = parseInt(_lastCode.substring(1)), secondChar, findIndex, code;
+  _.each(chars, function(char, index) {
+    if (char === _lastCode.substring(0, 1)) {
+      findIndex = index;
+    }
+  });
+  for (var i=findIndex; i < chars.length-1; i++) {
+    firstChar = chars[findIndex];
+    if (number < 9) {
+      for (var j=number; j < 9; j++) {
+        secondChar = "00" + (j+1).toString();
+        break;
+      }
+    } else if (number < 99) {
+      for (var j=number; j < 99; j++) {
+        secondChar = "0" + (j+1).toString();
+        break;
+      }
+    } else {
+      for (var j=number; j < 999; j++) {
+        secondChar = (j+1).toString();
+        break;
+      }
+    } 
+  }
+  return code = firstChar.concat(secondChar)
 }
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+function getBooking () {
+  var bookingQuery = new Parse.Query("Booking");
+      bookingQuery.ascending("code");
+  return  bookingQuery.find();
 }
 
 function createNewBooking(_userData, _params, _code) {
-  console.log(_userData);
   var Booking = Parse.Object.extend("Booking");
   var booking = new Booking();
-  var promise = new Parse.Promise();
   booking.set("user", _userData);
   booking.set("business", { __type: "Pointer", className: "Business", objectId: _params.BusinessId });
   booking.set("package", { __type: "Pointer", className: "Package", objectId: _params.PackageId });
@@ -70,11 +90,15 @@ function createNewBooking(_userData, _params, _code) {
   booking.set("isPaid", false);
   booking.set("numOfUsers", 1);
   booking.set("discount", null);
-  booking.save().then(
-    function(value) { promise.resolve(value); },
-    function(error) { promise.reject(error); }
-  )
-  return promise;
+  return booking.save();
+}
+
+function getRandomString() {
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+  var randomstring = '';
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  randomstring += chars.substring(randomNumber,randomNumber+1);
+  return randomstring;
 }
 
 Parse.Cloud.define('deleteUser', function(request, response) {
