9829a14883bf12bade1db1e2320c5c0ef9a26fdf
cong.nguyen <cong.nguyen@enouvo.com>
2017-03-17 16:09:43 +0700

    + add search discount function

---
+++ __tests__/models/discountModel.test.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+require('dotenv').config({path: "./.env"});
+var moments = require('moment');
+const Parse = require('parse/node');
+Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY , process.env.MASTER_KEY);
+Parse.serverURL = process.env.SERVER_URL;
+
+var DiscountModel = require('../../cloud/models/discountModel');
+
+describe('Discount Test', () => {
+    beforeAll (() => {
+      // create sample booking here
+    });
+
+    test.skip('Service Price', () => {
+      const date = new Date("2017-02-25T07:19:11.388Z");
+      return DiscountModel.getDiscountByTimeAndPackage("HOUR", date)
+      .then(function(result) {
+        console.log(result);
+      });
+    });
+
+    afterAll (() => {
+
+    });
+});
---
+++ cloud/models/discountModel.js
@@ -0,0 +1,25 @@
+var Tool    = require('./../utils/tools');
+var moments = require('moment');
+var _       = require('underscore');
+
+const Parse = require('parse/node');
+Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY , process.env.MASTER_KEY);
+Parse.serverURL = process.env.SERVER_URL
+
+function getDiscountByTimeAndPackage (_package, _time) {
+  return new Promise((resolve, reject) => {
+    var discountQuery = new Parse.Query("Discount");
+    discountQuery.lessThanOrEqualTo("startTime", _time);
+    discountQuery.greaterThanOrEqualTo("endTime", _time);
+    discountQuery.equalTo("status", "OPEN");
+    discountQuery.first().then(function(discounts) {
+      resolve(discounts);
+    }, function (error) {
+      reject(error);
+    });
+  }, function (error) {
+    reject(error);
+  });
+}
+
+exports.getDiscountByTimeAndPackage = getDiscountByTimeAndPackage;
