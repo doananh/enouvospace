3b38d9693f64176c45452d257a5a67dad1c6d4df
cong.nguyen <cong.nguyen@enouvo.com>
2017-03-21 15:42:17 +0700

    - validate checkin

--- cloud/checkin.js
+++ cloud/checkin.js
@@ -5,14 +5,18 @@ var BookingUtil = require('./models/bookingModel');
 
 Parse.Cloud.define("checkin", function(req, res) {
   var params = req.params;
-  if (params.UserId !== null) {
+  if (params.user && params.user.id && params.user.username) {
     BookingUtil.createBookingForLoginUser(params)
     .then(function (data) {
       return res.success({ status: 'checkin success'});
     }, function (error) {
       return res.error(error);
     });
-  } else {
+  }
+  else if (params.user && (!params.user.id || !params.user.username)) {
+    return res.error('Require id and username params for check in user');
+  }
+  else {
     BookingUtil.createBookingForAnonymousUser(params)
     .then(function (data) {
       return res.success(data);
