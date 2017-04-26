bd00b3d9662a8206968b23be10edc421a3f52be6
Quang <quang@Quangs-MacBook-Pro.local>
2017-04-26 15:36:45 +0700

    Remove type validation in package

--- cloud/validation/packageValidation.js
+++ cloud/validation/packageValidation.js
@@ -1,16 +1,9 @@
 var _ = require('underscore');
 
 Parse.Cloud.beforeSave("Package", function(req, res) {
-  var type        = req.object.get('type');
   var chargeRate  = req.object.get('chargeRate');
   var name        = req.object.get('name');
 
-  if (_.isUndefined(type) || _.isNull(type)) {
-    res.error('Required type params');
-  }
-  else if (type && !_.isString(type)) {
-    res.error('Invalid type format');
-  }
   if (_.isUndefined(name) || _.isNull(name)) {
     res.error('Required type params');
   }
