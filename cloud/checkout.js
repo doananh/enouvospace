20bb905275dafc32054f4dbd6e20691e1181ff67
cong.nguyen <cong.nguyen@enouvo.com>
2017-05-05 15:35:08 +0700

    * checkout by id

--- cloud/checkout.js
+++ cloud/checkout.js
@@ -52,37 +52,38 @@ Parse.Cloud.define("checkoutByBookingId", function(req, res) {
   var params = req.params;
   if (params && params.bookingId) {
     BookingModel.getBookingByParams({bookingId: params.bookingId})
-    .then(function (bookingData) {
-        return PriceCalculatingModel.getBookingPricingDetail(bookingData)
+    .then(function (bookingObject) {
+        return PriceCalculatingModel.calculateBookingPricing(bookingObject)
+        .then(function (data) {
+            bookingObject.set("payAmount", data.payAmount);
+            bookingObject.set("packageCount", data.packageCount);
+            bookingObject.set("numOfUsers", data.numOfUsers);
+            bookingObject.set("discountAmount", data.discountPricing.total);
+            bookingObject.set("serviceAmount", data.servicePricing.total);
+            bookingObject.set("package", data.packagePricing.package);
+            bookingObject.set("startTime", data.startTime);
+            bookingObject.set("endTime", data.endTime);
+            bookingObject.set("status", "CLOSED");
+            bookingObject.set("user", data.user);
+            return bookingObject.save();
+        })
+        .catch(function (error) {
+            return res.error(error);
+        });
+        /// temp write as a callback promise - will change later
     })
-    .then( function (formatData) {
-      var packageObject = {
-        name: formatData.package.name,
-        type: formatData.package.type,
-        chargeRate: formatData.package.chargeRate.value,
-        objectId: formatData.package.id
-      }
-      var bookingQuery = new Parse.Query('Booking');
-      bookingQuery.get(formatData.bookingId).then( function (booking) {
-        booking.set("payAmount", formatData.totalPrice.value);
-        booking.set("packageCount", formatData.package.count);
-        booking.set("numOfUsers", formatData.numOfUsers);
-        booking.set("discountAmount", formatData.discountAmount.value);
-        booking.set("serviceAmount", formatData.serviceAmount.value);
-        booking.set("package", packageObject);
-        booking.set("startTime", new Date(formatData.checkinTime));
-        booking.set("endTime", new Date(formatData.checkoutTime));
-        booking.set("status", "CLOSED");
-        booking.set("user", formatData.user);
-        // return booking.save();
-      })
-      .then ( function (saveResult) {
-        return res.success(formatData);
-      }, function (error) {
-        return res.error(error);
-      });
+    .then(function (bookingSaveResult) {
+        var recordParams = {
+          bookingId: bookingSaveResult.id,
+          username: bookingSaveResult.get('user').username,
+          userId: bookingSaveResult.get('user').id
+        }
+        return RecordModel.recordCheckout(recordParams)
     })
-    .catch( function (error) {
+    .then(function (recordData) {
+        return res.success(recordData);
+    })
+    .catch(function (error) {
         return res.error(error);
     });
   }
