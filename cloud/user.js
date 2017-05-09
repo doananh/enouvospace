66e75ab43756412b5afbcecb6ecf0463c304e4b0
cong.nguyen <cong.nguyen@enouvo.com>
2017-05-09 17:40:12 +0700

    - clean

--- cloud/user.js
+++ cloud/user.js
@@ -14,22 +14,23 @@ Parse.Cloud.define("updateUser", function(req, res) {
   var query = new Parse.Query(Parse.User);
   query.equalTo("objectId", params.objectId);
   query.first().then(function(currentUser) {
-    if (currentUser) {
-      currentUser.set('username', params.username);
-      currentUser.set('email', params.email);
-      currentUser.set('mobile', params.mobile);
-      currentUser.set('address', params.address);
-      currentUser.save(null, {useMasterKey: true}).then(function(user) {
-        res.success(user);
-      }, function(err) {
-        res.error(err);
-      });
-    } else {
-      res.error("update user err");
-    }
-  }, function(err) {
-    res.error(err);
-  });
+      if (currentUser) {
+        currentUser.set('username', params.username);
+        currentUser.set('email', params.email);
+        currentUser.set('mobile', params.mobile);
+        currentUser.set('address', params.address);
+        return currentUser.save(null, {useMasterKey: true})
+      }
+      else {
+        return res.error("update user err");
+      }
+  })
+  .then(function (saveResult) {
+      return res.success(saveResult);
+  })
+  .catch(function (error) {
+      return res.error(error);
+  })
 });
 
 Parse.Cloud.define('deleteUser', function(request, response) {
