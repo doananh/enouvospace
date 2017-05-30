4784ac0b363946037b2bd7c9d0dbcce976c8e2ff
cong.nguyen <cong.nguyen@enouvo.com>
2017-05-30 09:14:58 +0700

    * auto record checkout end of day

--- cloud/jobs/bookingReminding.js
+++ cloud/jobs/bookingReminding.js
@@ -3,17 +3,37 @@ Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY , process.env.MA
 Parse.serverURL = process.env.SERVER_URL;
 var CronJob = require('cron').CronJob;
 
-var checkoutEndOfDateJob = new CronJob({
-  cronTime: '00 22 00 * * 1-6',
+var recordCheckoutEndOfDateJob = new CronJob({
+  cronTime: '00 00 22 * * 1-6',
   onTick: function() {
-
+    Parse.Config.get().then(function(config) {
+        return config.get('AutoCheckoutWithInHours');
+    })
+    .then(function(AutoCheckoutWithInHours) {
+        var recordQuery = new Parse.Query('Record');
+        var d = new Date();
+        var time = AutoCheckoutWithInHours * 3600 * 1000;
+        var desTime = new Date(d.getTime() - (time));
+        recordQuery.greaterThanOrEqualTo('checkinTime', desTime);
+        recordQuery.each(function (record) {
+            record.set('checkoutTime', d);
+            return record.save();
+        })
+        .then(function (result) {
+            console.log('Auto record checkout success');
+        })
+        .catch(function (error) {
+            console.log('Auto record checkout failure');
+            console.log(error);
+        });
+    })
   },
   start: true,
   timeZone: 'Asia/Ho_Chi_Minh'
 });
 
 var bookingRemindingJob = new CronJob({
-  cronTime: '00 7 00 * * 1-6',
+  cronTime: '00 00 22 * * 1-6',
   onTick: function() {
 
   },
