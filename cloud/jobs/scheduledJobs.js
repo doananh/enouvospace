90d172b6ff821cc7fd676a12701d3b370d149849
cong.nguyen <cong.nguyen@enouvo.com>
2017-06-07 11:11:57 +0700

    + Add reset job for checkout record end of day at specific time

--- cloud/jobs/scheduledJobs.js
+++ cloud/jobs/scheduledJobs.js
@@ -1,37 +1,9 @@
-const Parse = require('parse/node');
-Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY , process.env.MASTER_KEY);
-Parse.serverURL = process.env.SERVER_URL;
 var CronJob = require('cron').CronJob;
 
 var recordCheckoutEndOfDateJob = new CronJob({
   cronTime: '00 00 23 * * 1-6',
   onTick: function() {
-    Parse.Config.get()
-    .then(function (config) {
-        return config.get('AutoCheckoutWithInHours');
-    })
-    .then(function (AutoCheckoutWithInHours) {
-        var recordQuery = new Parse.Query('Record');
-        var d = new Date();
-        var time = AutoCheckoutWithInHours * 3600 * 1000;
-        var desTime = new Date(d.getTime() - (time));
-        recordQuery.greaterThanOrEqualTo('checkinTime', desTime);
-        recordQuery.doesNotExist('checkoutTime');
-        recordQuery.each(function (record) {
-            record.set('checkoutTime', d);
-            return record.save();
-        })
-        .then(function (result) {
-            // console.log('Auto record checkout success');
-        })
-        .catch(function (error) {
-            // console.log('Auto record checkout failure');
-            // console.log(error);
-        });
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+    CheckoutRecordAutomatically();
   },
   start: true,
   timeZone: 'Asia/Ho_Chi_Minh'
@@ -45,3 +17,54 @@ var bookingRemindingJob = new CronJob({
   start: true,
   timeZone: 'Asia/Ho_Chi_Minh'
 });
+
+Parse.Cloud.job('resetRecordCheckoutEndOfDateJob', (req, res) => {
+  if (recordCheckoutEndOfDateJob.running) {
+    recordCheckoutEndOfDateJob.stop();
+  }
+  Parse.Config.get()
+  .then(function (config) {
+      var endOfDayCheckoutTime = config.get('endOfDayCheckoutTime');
+      var cronTime = "00 " + endOfDayCheckoutTime.minute + " " + endOfDayCheckoutTime.hour + "  * * 1-6";
+      var timeZone = endOfDayCheckoutTime.timeZone;
+      recordCheckoutEndOfDateJob = new CronJob({
+        cronTime: cronTime,
+        onTick: function() {
+          CheckoutRecordAutomatically();
+        },
+        start: false,
+        timeZone: timeZone
+      });
+      console.log('You have reset  End Of Day checkout Time at ' + endOfDayCheckoutTime.hour + ':' + endOfDayCheckoutTime.minute);
+      recordCheckoutEndOfDateJob.start();
+  });
+});
+
+function CheckoutRecordAutomatically () {
+  Parse.Config.get()
+  .then(function (config) {
+      return config.get('AutoCheckoutWithInHours');
+  })
+  .then(function (AutoCheckoutWithInHours) {
+      var recordQuery = new Parse.Query('Record');
+      var d = new Date();
+      var time = AutoCheckoutWithInHours * 3600 * 1000;
+      var desTime = new Date(d.getTime() - (time));
+      recordQuery.greaterThanOrEqualTo('checkinTime', desTime);
+      recordQuery.doesNotExist('checkoutTime');
+      recordQuery.each(function (record) {
+          record.set('checkoutTime', d);
+          return record.save();
+      })
+      .then(function (result) {
+          // console.log('Auto record checkout success');
+      })
+      .catch(function (error) {
+          // console.log('Auto record checkout failure');
+          // console.log(error);
+      });
+  })
+  .catch(function (error) {
+      console.log(error);
+  });
+}
