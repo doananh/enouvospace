574565d7cdc61ff52a42e30a97034484dd3df708
Minh Nguyen <minh.nguyen@enouvo.com>
2017-10-04 14:17:54 +0700

    Fix bug to display time in email shift

--- cloud/shift.js
+++ cloud/shift.js
@@ -5,7 +5,7 @@ var BookingModel  = require('./models/bookingModel.js');
 
 Parse.Cloud.define("sendEmailForShift", function(req, res) {
     var params = req.params;
-  	var subject = 'Statistic shift from start date time: '+moment(params.startDateTime).format("DD/MM/YYYY HH:mm")+' to end date time: '+moment(params.endDateTime).format("DD/MM/YYYY HH:mm");
+  	var subject = 'Statistic shift from start date time: '+moment(params.startDateTime).add(7, 'hours').format("DD/MM/YYYY HH:mm")+' to end date time: '+moment(params.endDateTime).add(7, 'hours').format("DD/MM/YYYY HH:mm");
     var content = '<p><b>Statistic Shift For Receptionist:</b>'+params.visitorName+'</p>'
 				+ '<p><b>- Total Number Of Record:</b>'+params.count+' records</p>'
 				+ '<p><b>- Total Price:</b>'+params.totalPrice+ ' VND'+'</p';				
